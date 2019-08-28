14aa4d93149e59172216d3c18c0d77a43fc9b044
Igor Malyarov <igor1309@gmail.com>
2019-08-29 00:45:19 +0300

    crazy filter works!!

--- BondsCashFlow/Views/EmissionList.swift
+++ BondsCashFlow/Views/EmissionList.swift
@@ -14,93 +14,27 @@ struct EmissionList: View {
     
     var local = true
     
-    @State private var filterType: FilterType = .all
+    @State private var preFilter: String = ""
+    @State private var filter: String = ""
+    @State private var filterType: FilterType = .withFlows
     
     private enum FilterType: String, CaseIterable {
-        case all, emitent, withFlows, complex
+        case all = "Все"
+        case withFlows = "С потоками"
+        //  MARK: TODO додумать и доделать
+        case emitent = "Эмитент"
+        case complex = "Текст"
     }
     
-    @State private var filteredEmissions: [EmissionStructure] = loadEmissionListData().sorted(by: {
-        (($0.emitentNameRus, $0.documentRus, $0.isinCode)
-            < ($1.emitentNameRus, $0.documentRus, $1.isinCode))
-    })
-    
-    //    private var emissionsWithFlows: [Int: Bool] = Dictionary(uniqueKeysWithValues: loadCashFlowListData().map { $0.emissionID }.removingDuplicates().map { ($0, true) })
-    //
-    @State private var filter: String = ""
-        {
-        didSet {
-            print("filter was set")
-            
-            switch filterType {
-            case .all:
-                filteredEmissions = userData.emissions.sorted(by: {
-                    (($0.emitentNameRus, $0.documentRus, $0.isinCode)
-                        < ($1.emitentNameRus, $0.documentRus, $1.isinCode))
-                })
-            case .emitent:
-                filteredEmissions = userData.emissions.filter({
-                    $0.emitentNameRus == filter
-                }).sorted(by: {
-                    (($0.emitentNameRus, $0.documentRus, $0.isinCode)
-                        < ($1.emitentNameRus, $0.documentRus, $1.isinCode))
-                })
-            case .complex:
-                filteredEmissions = userData.emissions.filter({
-                    $0.documentRus.contains(self.filter) ||
-                        $0.documentRus.contains(self.filter.uppercased()) ||
-                        $0.documentRus.contains(self.filter.lowercased()) ||
-                        $0.documentRus.contains(self.filter.capitalized) ||
-                        $0.isinCode.contains(self.filter) ||
-                        $0.isinCode.contains(self.filter.uppercased()) ||
-                        $0.isinCode.contains(self.filter.lowercased()) ||
-                        $0.isinCode.contains(self.filter.capitalized) ||
-                        $0.id == Int(filter) ?? -1
-                }).sorted(by: {
-                    (($0.emitentNameRus, $0.documentRus, $0.isinCode)
-                        < ($1.emitentNameRus, $0.documentRus, $1.isinCode))
-                })
-            case .withFlows:
-                filteredEmissions = userData.emissions
-                    .filter({ emission in
-                        userData.flows.map { $0.emissionID }.contains(emission.id)
-                    })
-                    .sorted(by: {
-                        (($0.emitentNameRus, $0.documentRus, $0.isinCode)
-                            < ($1.emitentNameRus, $0.documentRus, $1.isinCode))
-                    })
-            }
-            
-            emissionsCount = filteredEmissions.count
-            emitemtsCount = filteredEmissions.map({ $0.emitentID }).removingDuplicates().count
-        }
-    }
-    
-    
-    @State private var preFilter: String = ""
     @State private var showFilter = false
     
+    //  MARK: TODO доделать
     @State private var emissionsCount: Int = loadEmissionListData().count
     @State private var emitemtsCount: Int = loadEmissionListData().map({ $0.emitentID }).removingDuplicates().count
     
     var body: some View {
         NavigationView {
             VStack(alignment: .leading) {
-                TextField("Фильтр по названию выпуска, ISIN",
-                          text: $preFilter,
-                          onEditingChanged: { isEdited in
-                },
-                          onCommit: {
-                            if self.preFilter.count < 2 {
-                                self.filterType = .all
-                            } else {
-                                self.filterType = .complex
-                            }
-                            self.filter = self.preFilter
-                })
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding(.horizontal)
-                
                 Picker(selection: $filterType, label: Text("")) {
                     ForEach(FilterType.allCases, id: \.self) { type in
                         Text(type.rawValue).tag(type)
@@ -109,17 +43,61 @@ struct EmissionList: View {
                 .pickerStyle(SegmentedPickerStyle())
                 .padding(.horizontal)
                 
-                Text("\(emissionsCount.formattedGrouped) выпуск/а/ов, \(emitemtsCount.formattedGrouped) эмитент/а/ов")
-                    //  MARK: - одна из обций должна работать - не обрезать текст
-                    .lineLimit(nil)
-                    .fixedSize(horizontal: false, vertical: true)
-                    .foregroundColor(.secondary)
-                    .font(.subheadline)
-                    .padding(.horizontal)
+                if filterType == FilterType.complex {
+                    TextField("Фильтр по названию выпуска, ISIN",
+                              text: $preFilter,
+                              onEditingChanged: { isEdited in
+                    },
+                              onCommit: {
+                                if self.preFilter.count < 2 {
+                                    self.filterType = .all
+                                } else {
+                                    self.filterType = .complex
+                                }
+                                self.filter = self.preFilter
+                    })
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.horizontal)
+                }
+                
+
+                //  MARK: TODO доделать
+//                Text("\(emissionsCount.formattedGrouped) выпуск/а/ов, \(emitemtsCount.formattedGrouped) эмитент/а/ов")
+//                    //  MARK: - одна из опций должна работать (не обрезать текст)
+//                    .lineLimit(nil)
+//                    .fixedSize(horizontal: false, vertical: true)
+//                    .foregroundColor(.secondary)
+//                    .font(.subheadline)
+//                    .padding(.horizontal)
+                
                 
                 List {
                     //  apply filter if at least 2 symbols entered
-                    ForEach(filteredEmissions, id: \.self) { emission in
+                    ForEach(userData.emissions.filter({
+                        
+                        switch filterType {
+                        case .all:
+                            return true
+                        case .emitent:
+                            return $0.emitentNameRus == filter
+                        case .complex:
+                            return $0.documentRus.contains(self.filter) ||
+                                $0.documentRus.contains(self.filter.uppercased()) ||
+                                $0.documentRus.contains(self.filter.lowercased()) ||
+                                $0.documentRus.contains(self.filter.capitalized) ||
+                                $0.isinCode.contains(self.filter) ||
+                                $0.isinCode.contains(self.filter.uppercased()) ||
+                                $0.isinCode.contains(self.filter.lowercased()) ||
+                                $0.isinCode.contains(self.filter.capitalized) ||
+                                $0.id == Int(filter) ?? -1
+                        case .withFlows:
+                            return userData.flows.map { $0.emissionID }.contains($0.id)
+                        }
+                        
+                    }).sorted(by: {
+                        (($0.emitentNameRus, $0.documentRus, $0.isinCode)
+                            < ($1.emitentNameRus, $0.documentRus, $1.isinCode))
+                    }), id: \.self) { emission in
                         
                         EmissionRow(emission: emission)
                     }
